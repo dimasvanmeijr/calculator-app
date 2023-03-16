b08c9d852846ae31c229891280f30117a70fc866
dimazvanmeijr <dimasaryooz12@students.amikom.ac.id>
2023-03-16 14:28:23 +0700

    menambahkan file calculator.cs

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            int a = 10; // declarasi variabel a, dg nilai awal 10;
+            int b = 6; // declarasi variabel b, dg nilai awal 6;
+
+            Console.WriteLine("Hasil Penambahan " + a + " + "b + " = " + Penambahan(a, b));
+            Console.WriteLine(
+                
+                
+                
+                
+                
+                
+                
+                )
+            
+    }
+}
---
+++ CalculatorConsoleApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorConsoleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+        }
+
+        static void Main(string)[] args)
+        {
+        }
+            
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        } 
+
+        static int
+         
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                           
+}
